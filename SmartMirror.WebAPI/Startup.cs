6ff83670506374b3c6dc853be027b6ef5fcc12b6
noel <l12587@web.de>
2020-02-13 14:33:04 +0100

    test

--- SmartMirror.WebAPI/Startup.cs
+++ SmartMirror.WebAPI/Startup.cs
@@ -24,7 +24,7 @@ namespace SmartMirror.WebAPI
         {
             Configuration = configuration;
         }
-
+        //
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
