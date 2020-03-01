7521bee4266a32553785a5e5da2309ae63e3945e
Baris Tikir <brstkr@users.noreply.github.com>
2020-03-01 21:00:02 +0100

    Added GUI for "Universal Windows Platform (UWP)"

---
+++ SmartMirror.GUI_UWP/Clock/ClockModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMirror.GUI_UWP.Clock
+{
+    class ClockModel : BaseModel
+    {
+        DateTime currentTime;
+
+        public DateTime CurrentTime
+        {
+            get
+            {
+                return currentTime;
+            }
+            set
+            {
+                if (currentTime != value)
+                {
+                    currentTime = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public override TimeSpan Interval => TimeSpan.FromSeconds(1);
+
+        public override async Task Update()
+        {
+            CurrentTime = DateTime.Now;
+        }
+    }
+}
